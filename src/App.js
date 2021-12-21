25007a78c9a392f900c46dbd3422c27e33b24591
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-21 09:42:24 +0300

    register with complex password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes } from "react-router";
 import { Route } from "react-router";
-
-// import "./App.css";
+import Register from "./components/Register";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
